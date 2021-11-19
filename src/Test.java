ce21415f4b79dba79384d21eb1f725cf0c175bc9
Qaletx <330013129@qq.com>
2021-11-19 15:15:09 +0800

    2021/11/14进行版本控制的第三版，用于测试

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,46 @@
+import java.util.TreeMap;
+
 public class Test {
-    public static void main(String[] args) {
+    private int date;
+    private int year;
+    private int mooth;
+
+    public int getDate() {
+        return date;
     }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMooth() {
+        return mooth;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "date=" + date +
+                ", year=" + year +
+                ", mooth=" + mooth +
+                '}';
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setMooth(int mooth) {
+        this.mooth = mooth;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
     
 }
